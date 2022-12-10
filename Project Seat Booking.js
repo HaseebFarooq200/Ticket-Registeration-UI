b0c07ad2d3a41e629e2282754f2ec57175ce8599
Haseeb Khan <115168071+HaseebFarooq200@users.noreply.github.com>
2022-12-10 17:11:00 +0500

    Add files via upload

---
+++ Project Seat Booking.js
@@ -0,0 +1,27 @@
+console.log('Online Seat Booking')
+
+// function valid_name();
+// function valid_mail();
+// function valid_cnic();
+// function valid_seat();
+// function valid_dest();
+// function valid_busclass();
+// function valid_datetime();
+// function valid_phone();
+
+let my_name = document.getElementById('my_name')
+let e_mail = document.getElementById('my_mail')
+let cnic = document.getElementById('my_cnic')
+let seat = document.getElementById('myseat')
+let dest = document.getElementById('destination')
+let bus_class = document.getElementById('bus_class')
+let e_class = document.getElementById('eclass')
+let b_class = document.getElementById('bclass')
+let f_class = document.getElementById('fclass')
+let datetime = document.getElementById('datetime')
+let phone = document.getElementById('myphone')
+
+
+// console.log(my_name,e_mail,cnic,seat,dest,bus_class,datetime,phone);
+
+let submitbtn = document.getElementById('submitbtn');
